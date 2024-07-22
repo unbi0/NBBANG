fa94d4c3b3a62a6277d9f3940a00b796ccc62426
lchyeong <lchyeong@gmail.com>
2024-07-22 16:33:29 +0900

    chore: payment mvc 초기 세팅

---
+++ src/main/java/com/elice/nbbang/domain/payment/controller/PaymentController.java
@@ -0,0 +1,13 @@
+package com.elice.nbbang.domain.payment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/api/payment")
+@RequiredArgsConstructor
+@RestController
+public class PaymentController {
+
+}
---
+++ src/main/java/com/elice/nbbang/domain/payment/entity/Payment.java
@@ -0,0 +1,12 @@
+package com.elice.nbbang.domain.payment.entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+
+@Getter
+@Entity
+public class Payment {
+
+}
