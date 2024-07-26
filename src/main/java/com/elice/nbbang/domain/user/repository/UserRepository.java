03c4c0a3eae26fb6c9866558ed6887cf22ab5cac
[Cloud3] 최고은 <oh-my@nate.com>
2024-07-26 23:23:35 +0900

    del: 병합 전 user 도메인 삭제

--- src/main/java/com/elice/nbbang/domain/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elice.nbbang.domain.user.repository;
-
-import com.elice.nbbang.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
