3fc037c455917f6cb5c697d4b163751fc642d961
[Cloud3] 김현필 <gusvlf1028@naver.com>
2024-07-26 23:23:43 +0900

    remove: user

--- src/main/java/com/elice/nbbang/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elice.nbbang.domain.user.repository;
-
-import com.elice.nbbang.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
