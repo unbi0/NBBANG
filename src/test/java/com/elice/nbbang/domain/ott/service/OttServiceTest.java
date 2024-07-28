601223dcff5c6b07c4060369f8da2556ce9446c4
opixxx <opix0306@naver.com>
2024-07-28 13:27:26 +0900

    fix: 외래키 제약 조건으로 인한 삭제 오류 해결
    OttRepository 삭제를 위해 Ott 를 참조하고 있는 PartyRepository 먼저 삭제

--- src/test/java/com/elice/nbbang/domain/ott/service/OttServiceTest.java
+++ src/test/java/com/elice/nbbang/domain/ott/service/OttServiceTest.java
@@ -19,7 +19,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = "JASYPT_PASSWORD=1xladkagh")
 @SpringBootTest
 class OttServiceTest {
 
--- src/test/java/com/elice/nbbang/domain/party/service/PartyServiceTest.java
+++ src/test/java/com/elice/nbbang/domain/party/service/PartyServiceTest.java
@@ -1,19 +1,28 @@
 package com.elice.nbbang.domain.party.service;
 
+import static com.elice.nbbang.domain.user.entity.UserRole.USER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.elice.nbbang.domain.ott.entity.Ott;
 import com.elice.nbbang.domain.ott.repository.OttRepository;
 import com.elice.nbbang.domain.party.dto.PartyCreateRequest;
+import com.elice.nbbang.domain.party.repository.PartyRepository;
 import com.elice.nbbang.domain.user.entity.User;
 import com.elice.nbbang.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = "JASYPT_PASSWORD=1xladkagh")
 @SpringBootTest
 class PartyServiceTest {
+
+
+    @Autowired
+    private PartyRepository partyRepository;
 
     @Autowired
     private PartyService partyService;
@@ -24,12 +33,25 @@ class PartyServiceTest {
     @Autowired
     private OttRepository ottRepository;
 
+    @AfterEach
+    void tearDown() {
+        partyRepository.deleteAllInBatch();
+        ottRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
     @DisplayName("자신의 OTT 계정을 공유해 파티장으로 파티를 생성할 수 있다.")
     @Test
     void createParty() throws Exception {
         //given
         Ott ott = Ott.of("ChatGPT", 40000, 5);
-        User user = new User("홍길동", "1234");
+        User user = User.builder()
+                .email("mail@naver.com")
+                .nickname("test")
+                .phoneNumber("010-1234-5678")
+                .password("password")
+                .role(USER)
+                .build();
 
         ottRepository.save(ott);
         userRepository.save(user);
