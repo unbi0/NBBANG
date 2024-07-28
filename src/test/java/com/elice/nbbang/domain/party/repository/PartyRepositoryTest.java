1bec17a488acb8647ac81519244c5448dc5ebc70
opixxx <opix0306@naver.com>
2024-07-28 14:48:35 +0900

    style: 어노테이션 순서 교체

--- src/test/java/com/elice/nbbang/domain/party/repository/PartyRepositoryTest.java
+++ src/test/java/com/elice/nbbang/domain/party/repository/PartyRepositoryTest.java
@@ -17,8 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
 @TestPropertySource(properties = "JASYPT_PASSWORD=1xladkagh")
+@SpringBootTest
 class PartyRepositoryTest {
 
     @Autowired
