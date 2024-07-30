7a0514041b31923b22de7609b7bc0c0adf2c1a69
unao6 <oh-my@nate.com>
2024-07-30 16:51:18 +0900

    style: 잘못 들어온 securityConfig 코드 변경

--- src/main/java/com/elice/nbbang/global/config/SecurityConfig.java
+++ src/main/java/com/elice/nbbang/global/config/SecurityConfig.java
@@ -52,18 +52,15 @@ public class SecurityConfig {
                         .configurationSource(new CorsConfigurationSource() {
                             @Override
                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
                                 CorsConfiguration configuration = new CorsConfiguration();
 
                                 configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                                configuration.setAllowedMethods(
-                                        Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                                configuration.setAllowedMethods(Collections.singletonList(""));
+                                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                                 configuration.setAllowCredentials(true);
-                                configuration.setAllowedHeaders(Collections.singletonList(""));
+                                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
                                 configuration.setMaxAge(3600L);
-
                                 configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
                                 return configuration;
                             }
                         }));
@@ -77,31 +74,17 @@ public class SecurityConfig {
         //http basic 인증 방식 disable
         http
                 .httpBasic((auth) -> auth.disable());
-
-        // 임시 전체 허용
+        //경로별 인가 작업
         http
-<<<<<<< HEAD
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
-=======
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/api/users/sign-up", "/login", "/", "/api/users/user-login", "/api/users/check-email", "/api/users/email-certification", "/api/users/user-info").permitAll()  // 특정 경로 허용
                         .requestMatchers("'/api/users/**").hasRole("USER")  // USER 역할 필요
                         .requestMatchers("'/api/admin/**").hasRole("ADMIN")  // ADMIN 역할 필요
                         .requestMatchers("/reissue").permitAll()
-                        .requestMatchers("/api/**").permitAll()  // 모두 허용
+                        .requestMatchers("/ws/**").permitAll()  // WebSocket 엔드포인트 허용
                         .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
->>>>>>> feature/user
                 );
 
-        //경로별 인가 작업
-//        http
-//                .authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers("/api/users/sign-up", "/login", "/", "/api/users/user-login").permitAll()  // 특정 경로 허용
-//                        .requestMatchers("/admin").hasRole("ADMIN")  // ADMIN 역할 필요
-//                        .requestMatchers("/reissue").permitAll()
-//                        .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
-//                );
 
         http
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
