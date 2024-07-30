4df779a7e56e6146d0da07a152dbbd9a1e980b4e
유동원 <unbi04@naver.com>
2024-07-30 11:19:31 +0900

    구글로그인 추가

--- src/main/java/com/elice/nbbang/domain/user/dto/UserSignUpDto.java
+++ src/main/java/com/elice/nbbang/domain/user/dto/UserSignUpDto.java
@@ -1,11 +1,12 @@
 package com.elice.nbbang.domain.user.dto;
 
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserSignUpDto {
 
     private String email;
