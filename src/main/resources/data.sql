e5d43e8d74a31fa412f60c9b8f6bc2720e9b6174
opixxx <opix0306@naver.com>
2024-07-31 22:02:04 +0900

    refactor: 수정된 data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,24 @@
 -- OTT
-INSERT INTO ott (name, price, capacity, created_at, modified_at)
-VALUES ('Netflix', 15000, 4, NOW(), NOW());
+# INSERT INTO ott (name, price, capacity, created_at, modified_at)
+# VALUES ('넷플', 15000, 4, NOW(), NOW());
 
 INSERT INTO ott (name, price, capacity, created_at, modified_at)
-VALUES ('Disney+', 9900, 4, NOW(), NOW());
+VALUES ('디즈니플러스', 9900, 4, NOW(), NOW());
 
 INSERT INTO ott (name, price, capacity, created_at, modified_at)
-VALUES ('Amazon Prime', 12000, 6, NOW(), NOW());
+VALUES ('ChatGPT', 29900, 4, NOW(), NOW());
 
 INSERT INTO ott (name, price, capacity, created_at, modified_at)
-VALUES ('Hulu', 8000, 5, NOW(), NOW());
+VALUES ('티빙', 9900, 4, NOW(), NOW());
 
-INSERT INTO ott (name, price, capacity, created_at, modified_at)
-VALUES ('Apple TV+', 6500, 6, NOW(), NOW());
+# INSERT INTO ott (name, price, capacity, created_at, modified_at)
+# VALUES ('Amazon Prime', 12000, 6, NOW(), NOW());
+
+# INSERT INTO ott (name, price, capacity, created_at, modified_at)
+# VALUES ('Hulu', 8000, 5, NOW(), NOW());
+#
+# INSERT INTO ott (name, price, capacity, created_at, modified_at)
+# VALUES ('Apple TV+', 6500, 6, NOW(), NOW());
 
 INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
 VALUES ('user1', 'user1@example.com', 'password1', '010-1111-1111', 'USER', NOW(), NOW());
