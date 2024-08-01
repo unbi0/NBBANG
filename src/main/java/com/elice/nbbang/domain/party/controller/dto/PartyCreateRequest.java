f098923e842fedd106b80f9160af33a691895c89
opixxx <opix0306@naver.com>
2024-08-01 14:06:12 +0900

    refactor: UserUtil 을 통해 User 정보 가져오도록 변경

--- src/main/java/com/elice/nbbang/domain/party/controller/dto/PartyCreateRequest.java
+++ src/main/java/com/elice/nbbang/domain/party/controller/dto/PartyCreateRequest.java
@@ -4,14 +4,12 @@ import com.elice.nbbang.domain.party.service.dto.PartyCreateServiceRequest;
 
 public record PartyCreateRequest(
         Long ottId,
-        Long userId, // 시큐리티 구현이 되면 안받아도 될듯?
         String ottAccountId,
         String ottAccountPassword
 ) {
     public PartyCreateServiceRequest toServiceRequest() {
         return PartyCreateServiceRequest.builder()
                 .ottId(ottId)
-                .userId(userId)
                 .ottAccountId(ottAccountId)
                 .ottAccountPassword(ottAccountPassword)
                 .build();
--- src/main/java/com/elice/nbbang/domain/party/service/dto/PartyCreateServiceRequest.java
+++ src/main/java/com/elice/nbbang/domain/party/service/dto/PartyCreateServiceRequest.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 @Builder
 public record PartyCreateServiceRequest(
         Long ottId,
-        Long userId, // 시큐리티 구현이 되면 안받아도 될듯?
         String ottAccountId,
         String ottAccountPassword
 ) {
