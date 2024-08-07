0eb6e3a6640f26d7ab559a5f4f62d9fd023bfd01
gaeun2593@gmail.com <gaeun2593@gmail.com>
2024-08-07 22:01:04 +0900

    fix: 오버플로우 해결

--- src/main/java/com/elice/nbbang/domain/user/service/CustomUserDetailsService.java
+++ src/main/java/com/elice/nbbang/domain/user/service/CustomUserDetailsService.java
@@ -1,30 +1,30 @@
-//package com.elice.nbbang.domain.user.service;
-//
-//import com.elice.nbbang.domain.user.dto.CustomUserDetails;
-//import com.elice.nbbang.domain.user.entity.User;
-//import com.elice.nbbang.domain.user.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.DisabledException;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//
-//        User user = userRepository.findByEmail(email);
-//
-//        if (user.isDeleted()) {
-//            throw new DisabledException("이미 탈퇴된 계정입니다.");
-//        }
-//
-//        return new CustomUserDetails(user);
-//    }
-//}
+package com.elice.nbbang.domain.user.service;
+
+import com.elice.nbbang.domain.user.dto.CustomUserDetails;
+import com.elice.nbbang.domain.user.entity.User;
+import com.elice.nbbang.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email);
+
+        if (user.isDeleted()) {
+            throw new DisabledException("이미 탈퇴된 계정입니다.");
+        }
+
+        return new CustomUserDetails(user);
+    }
+}
