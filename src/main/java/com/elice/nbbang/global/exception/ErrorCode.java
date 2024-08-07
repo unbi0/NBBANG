8caf92badb00e1c649af2aa456870df84d2e8df1
opixxx <opix0306@naver.com>
2024-08-07 12:18:08 +0900

    feat: User ErrorCode 추가

--- src/main/java/com/elice/nbbang/global/exception/ErrorCode.java
+++ src/main/java/com/elice/nbbang/global/exception/ErrorCode.java
@@ -19,9 +19,10 @@ public enum ErrorCode {
     //Chat
     CHAT_NOT_FOUND(404, "채팅을 찾을 수 없습니다."),
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
-    MESSAGE_SEND_ERROR(500, "메세지 전송 중 오류가 발생했습니다.");
+    MESSAGE_SEND_ERROR(500, "메세지 전송 중 오류가 발생했습니다."),
 
-
+    //User
+    NOT_FOUND_USER(404, "조회된 USER 가 없습니다.");
     private final int status;
     private final String message;
 }
