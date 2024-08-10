02c36d642fcc521d3eded348dacfb2350bcb3197
gaeun2593@gmail.com <gaeun2593@gmail.com>
2024-08-10 23:00:15 +0900

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,12 +13,12 @@
 --
 -- INSERT INTO ott (name, price, capacity, created, modified)
 -- VALUES ('Apple TV+', 6500, 6, NOW(), NOW());
---
---INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
---VALUES ('user1', 'user1@example.com', 'password1', '010-1111-1111', 'USER', NOW(), NOW());
---
---INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
---VALUES ('user2', 'user2@example.com', 'password2', '010-2222-2222', 'ADMIN', NOW(), NOW());
---
---INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
---VALUES ('user3', 'user3@example.com', 'password3', '010-3333-3333', 'USER', NOW(), NOW());
+
+INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
+VALUES ('user1', 'user1@example.com', 'password1', '010-1111-1111', 'USER', NOW(), NOW());
+
+INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
+VALUES ('user2', 'user2@example.com', 'password2', '010-2222-2222', 'ADMIN', NOW(), NOW());
+
+INSERT INTO user (nickname, email, password, phone_number, role, created_at, modified_at)
+VALUES ('user3', 'user3@example.com', 'password3', '010-3333-3333', 'USER', NOW(), NOW());
