139e296dbc9510cb61f206d7f2453c481f8be9e5
unao6 <oh-my@nate.com>
2024-08-10 18:09:37 +0900

    feat: 결제 예정일 알림 기능 추가

--- src/main/java/com/elice/nbbang/domain/chat/controller/ChatController.java
+++ src/main/java/com/elice/nbbang/domain/chat/controller/ChatController.java
@@ -6,6 +6,7 @@ import com.elice.nbbang.domain.chat.service.ChatService;
 import com.elice.nbbang.domain.notification.controller.NotificationController;
 import com.elice.nbbang.domain.notification.dto.SmsRequest;
 import com.elice.nbbang.domain.notification.provider.NotificationSmsProvider;
+import com.elice.nbbang.domain.notification.scheduler.ExpirationNotificationScheduler;
 import com.elice.nbbang.domain.user.entity.User;
 import com.elice.nbbang.domain.user.service.UserService;
 import com.elice.nbbang.global.util.UserUtil;
@@ -23,7 +24,6 @@ public class ChatController{
     private final ChatService chatService;
     private final UserUtil userUtil;
     private final UserService userService;
-    private final NotificationSmsProvider notificationSmsProvider;
 
     @PostMapping("/start")
     public ResponseEntity<ChatResponse> startChat() {
@@ -32,12 +32,6 @@ public class ChatController{
 
         Long chatId = chatService.getOrCreateChat(user.getId());
         List<Message> messages = chatService.getChatMessages(chatId);
-
-        SmsRequest smsRequest = new SmsRequest(
-                user.getPhoneNumber(),
-                "안녕하세요!\nOTT 계정 공유 플랫폼'N/BBANG'입니다."
-        );
-        notificationSmsProvider.sendSms(smsRequest);
 
         return ResponseEntity.ok(new ChatResponse(chatId, messages));
     }
